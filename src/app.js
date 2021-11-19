54c2eeddc821c02278ec7ec33db2a29aa5f03a59
mpobletee <mpoblete95@gmail.com>
2021-11-18 22:24:55 -0300

    domain name generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  document.querySelector("#dominio").innerHTML = dominio();
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let dominio = () => {
+
+  let w = "www."
+  let pronoun = ["the", "our"];
+  let adjective = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dot = [".com", ".es", ".co", ".cl", ".net", ".org", ".uk"];
+  let newDom = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dot.length; l++) {
+          newDom.push(`${w}${pronoun[i]}${adjective[j]}${noun[k]}${dot[l]}<br/>`);
+        }
+      }
+    }
+  }
+  let newnewDom = newDom.join("");
+  return newnewDom;
 };
